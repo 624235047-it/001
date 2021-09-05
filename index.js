5ebcc87c2e99c3835d51600e968c65ccef11e36c
ja <624235047@parichat.skru.ac.th>
2021-09-05 21:34:01 +0700

    add

--- index.js
+++ index.js
@@ -5,94 +5,257 @@ var bodyParser = require('body-parser');
 
 //Firebase Real Time
 var firebase = require("firebase-admin");
-var serviceAccount = require("./book-shop.json");
+var serviceAccount = require("./firebase_key.json");
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
+	credential: firebase.credential.cert(serviceAccount),
 	databaseURL: "https://book-shop-2cc13-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
 var db = firebase.database();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/books',  function (req, res)  {  
+app.get('/books', function (req, res) {
 
 	res.setHeader('Content-Type', 'application/json');
 
 	var booksReference = db.ref("books");
 
 	//Attach an asynchronous callback to read the data
-	booksReference.on("value", 
-				function(snapshot) {					
-					res.json(snapshot.val());
-					booksReference.off("value");
-					}, 
-				function (errorObject) {
-					res.send("The read failed: " + errorObject.code);
-				});
-  
-});
-
-
-app.get('/topsellers',  function (req, res)  {  
-
-		res.setHeader('Content-Type', 'application/json');
-
-		var booksReference = db.ref("topsellers");
-	
-		//Attach an asynchronous callback to read the data
-		booksReference.on("value", 
-					function(snapshot) {					
-						res.json(snapshot.val());
-						booksReference.off("value");
-						}, 
-					function (errorObject) {
-						res.send("The read failed: " + errorObject.code);
-					});
-  
-});
-
-
-app.get('/book/:bookid',  function (req, res)  {  
-  	
-		//Code Here
+	booksReference.on("value",
+		function (snapshot) {
+			res.json(snapshot.val());
+			booksReference.off("value");
+		},
+		function (errorObject) {
+			res.send("The read failed: " + errorObject.code);
+		});
 
 });
 
-app.delete('/book/:bookid',  function (req, res)  {  
-  	
+app.get('/students', function (req, res) {
+
+	res.setHeader('Content-Type', 'application/json');
+
+	var booksReference = db.ref("students");
+
+	//Attach an asynchronous callback to read the data
+	booksReference.on("value",
+		function (snapshot) {
+			res.json(snapshot.val());
+			booksReference.off("value");
+		},
+		function (errorObject) {
+			res.send("The read failed: " + errorObject.code);
+		});
+
+});
+
+app.post('/plus', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+
+	var num1 = req.body.num1;
+	var num2 = req.body.num2;
+
+
+	res.send('{ "result": ' + (num1 + num2) + '}');
+});
+
+app.post('/book', function (req, res) {
+
+	var author = req.body.author;
+	var bookid = Number(req.body.bookid);
+	var category = req.body.category;
+	var isbn = req.body.isbn;
+	var pageCount = Number(req.body.pageCount);
+	var price = Number(req.body.price);
+	var publishedDate = req.body.publishedDate;
+	var shortDescription = req.body.shortDescription;
+	var thumbnailUrl = req.body.thumbnailUrl;
+	var title = req.body.title;
+
+	//console.log(author);
+
+	var referencePath = '/books/' + bookid + '/';
+
+	var booksReference = db.ref(referencePath);
+
+	if (booksReference != null) {
+
+		booksReference.update({
+			author: author, bookid: bookid, category: category, isbn: isbn, pageCount: pageCount,
+			price: price, publishedDate: publishedDate, shortDescription: shortDescription,
+			thumbnailUrl: thumbnailUrl, title: title
+		},
+			function (error) {
+				if (error) {
+					res.send("data could not be saved. " + error)
+				}
+				else {
+					res.send("Success");
+				}
+			});
+	}
+
+});
+
+app.post('/rectangle', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+
+	var width = req.body.width;
+	var long = req.body.long;
+
+	res.send('{"area": ' + (width * long) + '}');
+});
+
+app.post('/circle', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+
+	var radius = req.body.radius;
+
+	res.send('{"area": ' + (radius * radius * 3.14) + '}');
+});
+
+app.get('/topsellers', function (req, res) {
+
+	res.setHeader('Content-Type', 'application/json');
+
+	var booksReference = db.ref("topsellers");
+
+	//Attach an asynchronous callback to read the data
+	booksReference.on("value",
+		function (snapshot) {
+			res.json(snapshot.val());
+			booksReference.off("value");
+		},
+		function (errorObject) {
+			res.send("The read failed: " + errorObject.code);
+		});
+
+});
+
+
+app.get('/book/:bookid', function (req, res) {
+
 	//Code Here
+	res.setHeader('Content-Type', 'application/json');
+	var bookid = Number(req.params.bookid);
 
-	
+	var booksReference = db.ref("books")
+
+	booksReference.orderByChild("bookid").equalTo(bookid).on("child_added",
+		function (snapshot) {
+			res.json(snapshot.val());
+			booksReference.off("value");
+		},
+		function (errorObject) {
+			res.send("The read failed: " + errorObject.code);
+		});
+});
+
+app.get('/student/:studentId', function (req, res) {
+
+	//Code Here
+	res.setHeader('Content-Type', 'application/json');
+	var studentId = req.params.studentId;
+
+	var booksReference = db.ref("students")
+
+	booksReference.orderByChild("studentId").equalTo(studentId).on("child_added",
+		function (snapshot) {
+			res.json(snapshot.val());
+			booksReference.off("value");
+		},
+		function (errorObject) {
+			res.send("The read failed: " + errorObject.code);
+		});
+});
+
+app.post('/student', function (req, res) {
+
+	var studentId = Number(req.body.studentId);
+	var studentName = req.body.studentName;
+
+	var referencePath = '/students/' + studentId + '/';
+
+	var studentsReference = db.ref(referencePath);
+
+	if (studentsReference != null) {
+
+		studentsReference.update({
+			studentId: studentId, studentName: studentName
+		},
+			function (error) {
+				if (error) {
+					res.send("data could not be saved. " + error)
+				}
+				else {
+					res.send("Success");
+				}
+			});
+	}
+});	
+
+app.delete('/student/:studentId', function (req, res) {
+
+	var studentId = Number(req.params.studentId);
+
+	var referencePath = '/students/' + studentId + '/';
+	var studentsReference = db.ref(referencePath);
+
+	if (studentsReference != null) {
+		studentsReference.remove()
+		return res.send("Success");
+	}
+
+	if (error) throw error;
+
+});
+
+app.delete('/book/:bookid', function (req, res) {
+
+	//Code Here
+	//res.setHeader('Content-Type', 'application/json');
+	var bookid = Number(req.params.bookid);
+
+	var referencePath = '/books/' + bookid + '/';
+	var booksReference = db.ref(referencePath);
+
+	if (booksReference != null) {
+		booksReference.remove()
+		return res.send("Success");
+	}
+
+	if (error) throw error;
 
 });
 
 
-app.get('/lastorderid',  function (req, res)  {  
-  	
+app.get('/lastorderid', function (req, res) {
+
 	res.setHeader('Content-Type', 'application/json');
 
 	var ordersReference = db.ref("lastOrderId");
 
-	ordersReference.on("value", 
-				function(snapshot) {					
-					res.json(snapshot.val());
-					ordersReference.off("value");
-					}, 
-				function (errorObject) {
-					res.send("The read failed: " + errorObject.code);
-			});
+	ordersReference.on("value",
+		function (snapshot) {
+			res.json(snapshot.val());
+			ordersReference.off("value");
+		},
+		function (errorObject) {
+			res.send("The read failed: " + errorObject.code);
+		});
 
 });
 
 
-app.put('/lastorderid',  function (req, res)  {  
-	
+app.put('/lastorderid', function (req, res) {
+
 	//Code Here
 
 
@@ -101,7 +264,7 @@ app.put('/lastorderid',  function (req, res)  {
 
 
 
-app.post('/order',  function (req, res)  {  
+app.post('/order', function (req, res) {
 
 	//Code Here
 
@@ -109,5 +272,5 @@ app.post('/order',  function (req, res)  {
 
 
 app.listen(port, function () {
-    console.log("Server is up and running...");
+	console.log("Server is up and running...");
 });
